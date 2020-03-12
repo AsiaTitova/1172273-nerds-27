753de79a1e807ee5b20a57c78c913571219027c5
Asia Titova <Kutsanastasia@yandex.ru>
2020-03-13 00:58:30 +0700

    правки

--- js/slider-script.js
+++ js/slider-script.js
@@ -12,22 +12,32 @@ slidebuttonone.addEventListener("click", function (evt) {
     
     evt.preventDefault();
     slideone.classList.toggle("visually-hidden");
+    slidebuttonone.classList.add("slide-button-active");
     slidetwo.classList.add("visually-hidden");
+    slidebuttontwo.classList.remove("slide-button-active");
     slidethree.classList.add("visually-hidden");
+    slidebuttonthree.classList.remove("slide-button-active");
 });
 
 slidebuttontwo.addEventListener("click", function (evt) {
     
     evt.preventDefault();
     slideone.classList.add("visually-hidden");
+    slidebuttonone.classList.remove("slide-button-active");
     slidetwo.classList.toggle("visually-hidden");
+    slidebuttontwo.classList.add("slide-button-active");
     slidethree.classList.add("visually-hidden");
+    slidebuttonthree.classList.remove("slide-button-active");
 });
 
 slidebuttonthree.addEventListener("click", function (evt) {
     
     evt.preventDefault();
     slideone.classList.add("visually-hidden");
+    slidebuttonone.classList.remove("slide-button-active");
     slidetwo.classList.add("visually-hidden");
+    slidebuttontwo.classList.remove("slide-button-active");
     slidethree.classList.toggle("visually-hidden");
+    slidebuttonthree.classList.add("slide-button-active");
 });
+
